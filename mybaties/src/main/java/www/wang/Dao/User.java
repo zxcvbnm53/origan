9a700378cc4b2aa466c5d57d255830352a4c829a
practice <3147880185@qq.com>
2023-03-10 12:36:00 +0800

    practice for Mybaties

---
+++ mybaties/src/main/java/www/wang/Dao/User.java
@@ -0,0 +1,93 @@
+package www.wang.Dao;
+
+public class User {
+    private String userId;
+    private String cardType;
+    private String cardNo;
+    private String userName;
+    private String userSex;
+    private String userAge;
+    private String userRole;
+
+    public User() {
+    }
+
+    public User(String userId, String cardType, String cardNo, String userName, String userSex, String userAge, String userRole) {
+        this.userId = userId;
+        this.cardType = cardType;
+        this.cardNo = cardNo;
+        this.userName = userName;
+        this.userSex = userSex;
+        this.userAge = userAge;
+        this.userRole = userRole;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCardNo() {
+        return cardNo;
+    }
+
+    public void setCardNo(String cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", cardType='" + cardType + '\'' +
+                ", cardNo='" + cardNo + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userSex='" + userSex + '\'' +
+                ", userAge='" + userAge + '\'' +
+                ", userRole='" + userRole + '\'' +
+                '}';
+    }
+}
